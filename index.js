94c49217da5f4bf0ca5af4624290a731ee3ab692
dixoncode <dicksonabuka@outlook.com>
2024-09-01 15:41:47 +0300

    Adding leading zeros to the seconds and minutes

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ let secondsElapsed = 0;
 let interval = null;
 const time = document.getElementById("time")
 
+function padStart(value) {
+    return String(value).padStart(2,"0")
+}
+
 function setTime() {
     const minutes = Math.floor(secondsElapsed/60)
     const seconds = secondsElapsed % 60
-    time.innerHTML = `${minutes}:${seconds}`;
+    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`;
 }
 
 function timer() {
